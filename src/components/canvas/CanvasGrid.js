035102b0ff9f92ac04238778a910edb031c5b941
Andy Tan <andytan0727@gmail.com>
2019-01-20 18:37:24 +0800

    Refactor to stateles component (No cycle methods)

--- src/components/canvas/CanvasGrid.js
+++ src/components/canvas/CanvasGrid.js
@@ -4,42 +4,42 @@ import { Layer, Group, Rect, Text } from "react-konva";
 const darkRed = "#ff7373";
 const paleRed = "#ffe4e1";
 
-export default class CanvasGrid extends React.PureComponent {
-  render() {
-    const {
-      layout,
-      box: { width: boxWidth, height: boxHeight }
-    } = this.props.grid;
+const CanvasGrid = props => {
+  const {
+    layout,
+    box: { width: boxWidth, height: boxHeight }
+  } = props.grid;
 
-    return (
-      <Layer>
-        {Object.keys(layout).map(box => {
-          const isEven = !(box % 2);
+  return (
+    <Layer>
+      {Object.keys(layout).map(box => {
+        const isEven = !(box % 2);
 
-          return (
-            <Group key={`box_${box}`}>
-              <Rect
-                x={Math.floor(layout[box].x - boxWidth / 2)}
-                y={Math.floor(layout[box].y - boxWidth / 2)}
-                width={boxWidth}
-                height={boxHeight}
-                fill={isEven ? darkRed : paleRed}
-                perfectDrawEnabled={false}
-              />
-              <Text
-                x={Math.floor(layout[box].x - boxWidth / 2)}
-                y={Math.floor(layout[box].y - boxWidth / 2)}
-                fill={isEven ? paleRed : darkRed}
-                text={box}
-                padding={4}
-                fontSize={16}
-                fontFamily={'arial'}
-                perfectDrawEnabled={false}
-              />
-            </Group>
-          );
-        })}
-      </Layer>
-    );
-  }
-}
+        return (
+          <Group key={`box_${box}`}>
+            <Rect
+              x={Math.floor(layout[box].x - boxWidth / 2)}
+              y={Math.floor(layout[box].y - boxWidth / 2)}
+              width={boxWidth}
+              height={boxHeight}
+              fill={isEven ? darkRed : paleRed}
+              perfectDrawEnabled={false}
+            />
+            <Text
+              x={Math.floor(layout[box].x - boxWidth / 2)}
+              y={Math.floor(layout[box].y - boxWidth / 2)}
+              fill={isEven ? paleRed : darkRed}
+              text={box}
+              padding={4}
+              fontSize={16}
+              fontFamily={"arial"}
+              perfectDrawEnabled={false}
+            />
+          </Group>
+        );
+      })}
+    </Layer>
+  );
+};
+
+export default CanvasGrid;
