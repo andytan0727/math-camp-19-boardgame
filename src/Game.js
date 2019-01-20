63c98e2fdfec4e161d1a58ea4c490d810a136a9c
Andy Tan <andytan0727@gmail.com>
2019-01-20 13:08:27 +0800

    Added main board grid.

--- src/Game.js
+++ src/Game.js
@@ -1,33 +1,60 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Stage } from 'react-konva';
+
+// Imported actionsCreators
 import { startGame } from "./actions/gameActions";
+import { addNewPlayer } from "./actions/playerActions";
+
+// Import canvas components
+import CanvasGrid from './components/canvas/CanvasGrid';
+
+
 import "./Game.css";
+import { Canvas } from "konva";
 
 class Game extends Component {
   render() {
+    // States
+    const { width, height } = this.props.board;
     const { gameNum } = this.props.game;
-    const { startGame } = this.props;
+    const { count, current, all: allPlayers } = this.props.players;
+
+    // Actions
+    const { startGame, addNewPlayer } = this.props;
+
     return (
       <div className="Game">
-        <h1>Math Camp Board Game</h1>
-        <p onClick={startGame}>
-          This game is {gameNum}.
-        </p>
+        <React.Fragment>
+          <div id={'main-board'}>
+            <Stage width={width} height={height}>
+              <CanvasGrid grid={this.props.board}>
+
+              </CanvasGrid>
+            </Stage>
+          </div>
+        </React.Fragment>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { game } = state;
+  const { board, game, players } = state;
   return {
-    game
+    board,
+    game,
+    players
   };
 };
 
 export default connect(
   mapStateToProps,
-  {
-    startGame
+  { // mapDispatchToProps object literal
+    // game
+    startGame,
+
+    // players
+    addNewPlayer
   }
 )(Game);
