281d7741baa8b435408c447843b554f2a5b53694
Andy Tan <andytan0727@gmail.com>
2019-01-20 12:37:49 +0800

    Added board to Redux state.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { board } from './boardReducer';
 import { game } from './gameReducer'
 import { players } from './playerReducer';
 
 export default combineReducers({
+  board,
   game,
   players
 });
---
+++ src/utils/boardHelpers.js
@@ -0,0 +1,42 @@
+export const getLayout = dimensions => {
+  const {
+    GRID_WIDTH: gridWidth,
+    GRID_HEIGHT: gridHeight,
+    BOX_WIDTH: boxWidth,
+    BOX_HEIGHT: boxHeight
+  } = dimensions;
+
+  // Make a 300 rows game
+  let layout = {};
+  const oddRows = [];
+  const evenRows = [];
+  for (let i = 0; i <= 29; i++) {
+    if (i % 2) {
+      oddRows.push(i);
+    } else {
+      evenRows.push(i);
+    }
+  }
+
+  // Main logic for grid drawing
+  for (let col = 1; col <= 10; col++) {
+    // Even rows numbering go from left to right
+    evenRows.forEach(row => {
+      layout[row * 10 + col] = {
+        x: Math.floor((col - 1) * boxWidth + boxWidth / 2),
+        y: Math.floor(gridHeight - (row * boxHeight + boxHeight / 2)),
+        id: col + 10 * row
+      };
+    });
+
+    // Odd rows numbering go from right to left
+    oddRows.forEach(row => {
+      layout[col + 10 * row] = {
+        x: Math.floor(gridWidth - ((col - 1) * boxWidth + boxWidth / 2)),
+        y: Math.floor(gridHeight - (row * boxHeight + boxHeight / 2)),
+        id: col + 10 * row
+      };
+    });
+  }
+  return layout;
+};
