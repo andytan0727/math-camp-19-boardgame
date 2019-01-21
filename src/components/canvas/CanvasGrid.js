d2a24a2d3aab4c01ba9e98a772b2bc3c6bee3eb6
Andy Tan <andytan0727@gmail.com>
2019-01-21 13:37:19 +0800

    Destroy layer after unmount.

--- src/components/canvas/CanvasGrid.js
+++ src/components/canvas/CanvasGrid.js
@@ -4,42 +4,50 @@ import { Layer, Group, Rect, Text } from "react-konva";
 const darkRed = "#ff7373";
 const paleRed = "#ffe4e1";
 
-const CanvasGrid = props => {
-  const {
-    layout,
-    box: { width: boxWidth, height: boxHeight }
-  } = props.grid;
+export default class CanvasGrid extends React.Component {
+  componentDidMount() {
+  }
 
-  return (
-    <Layer>
-      {Object.keys(layout).map(box => {
-        const isEven = !(box % 2);
+  componentWillUnmount() {
+    console.log('Grid unmounted!');
+    this.gridLayer.destroy();
+  }
 
-        return (
-          <Group key={`box_${box}`}>
-            <Rect
-              x={Math.floor(layout[box].x - boxWidth / 2)}
-              y={Math.floor(layout[box].y - boxWidth / 2)}
-              width={boxWidth}
-              height={boxHeight}
-              fill={isEven ? darkRed : paleRed}
-              perfectDrawEnabled={false}
-            />
-            <Text
-              x={Math.floor(layout[box].x - boxWidth / 2)}
-              y={Math.floor(layout[box].y - boxWidth / 2)}
-              fill={isEven ? paleRed : darkRed}
-              text={box}
-              padding={4}
-              fontSize={16}
-              fontFamily={"arial"}
-              perfectDrawEnabled={false}
-            />
-          </Group>
-        );
-      })}
-    </Layer>
-  );
-};
+  render() {
+    const {
+      layout,
+      box: { width: boxWidth, height: boxHeight }
+    } = this.props.grid;
 
-export default CanvasGrid;
+    return (
+      <Layer ref={node => (this.gridLayer= node)}>
+        {Object.keys(layout).map(box => {
+          const isEven = !(box % 2);
+
+          return (
+            <Group key={`box_${box}`}>
+              <Rect
+                x={Math.floor(layout[box].x - boxWidth / 2)}
+                y={Math.floor(layout[box].y - boxWidth / 2)}
+                width={boxWidth}
+                height={boxHeight}
+                fill={isEven ? darkRed : paleRed}
+                perfectDrawEnabled={false}
+              />
+              <Text
+                x={Math.floor(layout[box].x - boxWidth / 2)}
+                y={Math.floor(layout[box].y - boxWidth / 2)}
+                fill={isEven ? paleRed : darkRed}
+                text={box}
+                padding={4}
+                fontSize={16}
+                fontFamily={"arial"}
+                perfectDrawEnabled={false}
+              />
+            </Group>
+          );
+        })}
+      </Layer>
+    );
+  }
+}
