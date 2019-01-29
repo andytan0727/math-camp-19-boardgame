f0a182b71384db154d60114a28b93b81ac2afbd8
Andy Tan <andytan0727@gmail.com>
2019-01-29 14:18:04 +0800

    Added call to splash screen before main content fully loaded.

--- public/electron.js
+++ public/electron.js
@@ -7,10 +7,12 @@ const isDev = require("electron-is-dev");
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let splashScreen;
+const winParams = { width: 1100, height: 800, show: false };
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1100, height: 800 });
+  mainWindow = new BrowserWindow(winParams);
 
   // and load the index.html of the app.
   const startUrl = isDev
@@ -22,9 +24,24 @@ function createWindow() {
       });
   mainWindow.loadURL(startUrl);
 
+  mainWindow.webContents.on("did-finish-load", () => {
+    mainWindow.show();
+
+    if (splashScreen) {
+      let loadingScreenBounds = splashScreen.getBounds();
+      mainWindow.setBounds(loadingScreenBounds);
+      splashScreen.close();
+    }
+  });
+
+  // Open devtools in development environment
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
   // Download and install extensions
   // on development environment
-  if (process.env.NODE_ENV === "development") {
+  if (isDev) {
     const {
       default: installExtension,
       REACT_DEVELOPER_TOOLS,
@@ -41,13 +58,38 @@ function createWindow() {
   }
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", function() {
+  mainWindow.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
   });
 }
+
+const createLoadingScreen = () => {
+  splashScreen = new BrowserWindow({
+    ...winParams,
+    parent: mainWindow
+  });
+  splashScreen.loadURL(
+    isDev
+      ? url.format({
+          pathname: path.join(__dirname, "/splash.html"),
+          protocol: "file:",
+          slashes: true
+        })
+      : url.format({
+          pathname: path.join(__dirname, "../build/splash.html"),
+          protocol: "file:",
+          slashes: true
+        })
+  );
+
+  splashScreen.on("closed", () => (splashScreen = null));
+  splashScreen.webContents.on("did-finish-load", () => {
+    splashScreen.show();
+  });
+};
 
 const generateMenu = () => {
   const template = [
@@ -117,12 +159,13 @@ const generateMenu = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", () => {
+  createLoadingScreen();
   createWindow();
   generateMenu();
 });
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
@@ -130,7 +173,7 @@ app.on("window-all-closed", function() {
   }
 });
 
-app.on("activate", function() {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
