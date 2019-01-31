8a45377a561c35a8727788f590d379f86bb4218b
Andy Tan <andytan0727@gmail.com>
2019-01-31 18:14:03 +0800

    Refactor animated.Text to render props.

--- src/components/canvas/CanvasPlayer.tsx
+++ src/components/canvas/CanvasPlayer.tsx
@@ -13,16 +13,81 @@ import styles from "../../styles/CanvasPlayer.module.css";
 import { ILayout, ITileDim } from "../../store/board/types";
 import { ISinglePlayerObj } from "../../store/player/types";
 
-interface ConnectedProps {
+interface ICoord {
+  x: number;
+  y: number;
+}
+interface ICoordAndWidth {
+  coord: ICoord;
+  width: number;
+}
+
+interface DimensionTextProps extends ICoordAndWidth {
+  id: number;
+  color: string;
+}
+interface PlayerToAnimateProps extends ICoordAndWidth {
+  id: number;
+  color: string;
+  scale: number;
+  render: ({ id, width, coord, color }: DimensionTextProps) => React.ReactNode;
+}
+
+interface CanvasPlayerProps {
   player: ISinglePlayerObj;
   current: ISinglePlayerObj;
   layout: ILayout;
   box: ITileDim;
 }
 
-interface OwnProps {}
+const DimensionText = (props: DimensionTextProps) => {
+  const { id, coord, width, color } = props;
+  let x: number;
+  let y: number;
+  let fontSize: number;
 
-type CanvasPlayerProps = ConnectedProps & OwnProps;
+  if (width / 2 > 30) {
+    x = id % 10 !== 0 ? coord.x - 7 : coord.x - 14.5;
+    y = coord.y - 11;
+    fontSize = 23;
+  } else {
+    x = id % 10 !== 0 ? (id === 1 ? coord.x - 5 : coord.x - 4) : coord.x - 9;
+    y = coord.y - 7;
+    fontSize = 14;
+  }
+
+  return (
+    <animated.Text
+      x={x}
+      y={y}
+      fill={color}
+      text={id.toString()}
+      fontSize={fontSize}
+      fontFamily={"arial"}
+      fontStyle={"Bold"}
+      perfectDrawEnabled={false}
+    />
+  );
+};
+
+const PlayerToAnimate = (props: PlayerToAnimateProps) => {
+  const { id, coord, width, color, scale, render } = props;
+  return (
+    <animated.Group>
+      <animated.Circle
+        x={coord.x}
+        y={coord.y}
+        radius={width / 2 - 10}
+        fill={styles.white}
+        scale={{ x: scale, y: scale }}
+        stroke={color}
+        strokeWidth={4}
+        perfectDrawEnabled={false}
+      />
+      {render({ id, width, coord, color })}
+    </animated.Group>
+  );
+};
 
 export default class CanvasPlayer extends React.Component<
   CanvasPlayerProps,
@@ -31,40 +96,40 @@ export default class CanvasPlayer extends React.Component<
   render() {
     const {
       player: { id, pos, color },
-      current: { id: currentPlayerId },
+      // current: { id: currentPlayerId },
       layout,
       box,
       box: { width }
     } = this.props;
     const grid = { layout, box };
 
-    const { x, y } = getPlayerCoordinates(pos.toString(), grid);
-    const isCurrent = !!(id === currentPlayerId);
+    const coord = getPlayerCoordinates(pos.toString(), grid);
+    const { x, y } = coord;
+    // const isCurrent = !!(id === currentPlayerId);
 
     return (
       <Layer>
-        <Spring to={{ x, y }}>
-          {({ x, y }) => (
-            <animated.Group>
-              <animated.Circle
-                x={x}
-                y={y}
-                radius={width / 2 - 10}
-                fill={isCurrent ? styles.white : color}
-                stroke={color}
-                strokeWidth={2}
-                perfectDrawEnabled={false}
-              />
-              <animated.Text
-                x={id % 10 !== 0 ? x - 5 : x - 11}
-                y={y - 7}
-                fill={isCurrent ? color : styles.white}
-                text={id.toString()}
-                fontSize={18}
-                fontFamily={"arial"}
-                perfectDrawEnabled={false}
-              />
-            </animated.Group>
+        <Spring
+          from={{ scale: 1 }}
+          to={{ x, y, scale: 1.5 }}
+          after={{ scale: 1 }}
+        >
+          {({ x, y, scale }) => (
+            <PlayerToAnimate
+              id={id}
+              coord={{ x, y }}
+              width={width}
+              color={color}
+              scale={scale}
+              render={({ id, width, coord, color }) => (
+                <DimensionText
+                  id={id}
+                  width={width}
+                  coord={coord}
+                  color={color}
+                />
+              )}
+            />
           )}
         </Spring>
       </Layer>
