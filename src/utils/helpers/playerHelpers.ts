2da4b5fa213ec3b36355e6f9eb93f8a20d34ccaa
Andy Tan <andytan0727@gmail.com>
2019-02-02 13:48:44 +0800

    Remove unwanted method and update players' colors.

--- src/utils/helpers/playerHelpers.ts
+++ src/utils/helpers/playerHelpers.ts
@@ -1,26 +1,20 @@
 // Interfaces
-import {
-  ISinglePlayerObj,
-  IScores,
-  IScoresAll,
-  IUpdatedData
-} from "../../store/player/types";
+import { ISinglePlayerObj } from "../../store/player/types";
 import { ILayout, ITileDim } from "../../store/board/types";
 
 export const colorPalette = [
-  "#5f6977",
-  "#997f6c",
-  "#7f5954",
-  "#d8b477",
-  "#789960",
-  "#a7db64",
-  "#3271ad",
-  "#4c4096",
-  "#c14188",
-  "#8c3c88"
+  "red",
+  "orange",
+  "grey",
+  "olive",
+  "green",
+  "teal",
+  "blue",
+  "violet",
+  "purple",
+  "brown"
 ];
 
-// Interfaces
 interface grid {
   layout: ILayout;
   box: ITileDim;
@@ -43,16 +37,11 @@ export const generatePlayer = (curPlayerId: number): ISinglePlayerObj => ({
       score: 0,
       extra: 0
     }
-  ],
+  ]
 });
 
-export const getPlayerCoordinates = (
-  pos: string,
-  grid: grid,
-) => {
-  const {
-    layout,
-  } = grid;
+export const getPlayerCoordinates = (pos: string, grid: grid) => {
+  const { layout } = grid;
 
   const x = layout[pos].x;
   const y = layout[pos].y;
@@ -63,131 +52,9 @@ export const getPlayerCoordinates = (
   };
 };
 
-/**
- *
- * @param datFromFile Data from JSON file
- * @param curId from player
- *
- * @returns An Object {
- *   current: {
- *     game: [
- *       {
- *         score: number,
- *         extra: number
- *       },
- *       {
- *         score: number,
- *         extra: number
- *       },
- *       ...,
- *       {
- *         score: number,
- *         extra: number
- *       }
- *     ]
- *   },
- *   all: [
- *     {
- *       id: number,
- *       game: [
- *         { score: number, extra: number },
- *   5x    { score: number, extra: number },
- *         ...,
- *         { score: number, extra: number }
- *       ]
- *     },
- *     {
- *       id: number,
- *       game: [
- *         { score: number, extra: number },
- *   5x    { score: number, extra: number },
- *         ...,
- *         { score: number, extra: number }
- *       ]
- *     },
- *     {
- *       id: number,
- *       game: [
- *         { score: number, extra: number },
- *   5x    { score: number, extra: number },
- *         ...,
- *         { score: number, extra: number }
- *       ]
- *     },
- *     ...,
- *     {
- *       id: number,
- *       game: [
- *         { score: number, extra: number },
- *   5x    { score: number, extra: number },
- *         ...,
- *         { score: number, extra: number }
- *       ]
- *     }
- *   ]
- * }
- */
-export const updatePlayerScores = (
-  datFromFile: Array<IScoresAll>,
-  curId: number
-) => {
-  let updatedData: IUpdatedData = {
-    current: {
-      id: 1,
-      game: []
-    },
-    all: []
-  };
-
-  // Populating updatedData.all with data from JSON
-  for (const game of Array.from(datFromFile)) {
-    const { score, extra } = game;
-
-    // Update scores
-    for (let i = 0; i < score.length; i++) {
-      const curPlayerGameVal: IScores = {
-        score: score[i],
-        extra: extra[i]
-      };
-
-      updatedData = !(
-        updatedData.all &&
-        updatedData.all[i] &&
-        updatedData.all[i].game
-      )
-        ? {
-            ...updatedData,
-            all: [
-              ...updatedData.all,
-              {
-                id: i + 1,
-                game: [curPlayerGameVal]
-              }
-            ]
-          }
-        : {
-            ...updatedData,
-            all: updatedData.all.map(player => {
-              return player.id === i + 1
-                ? {
-                    id: i + 1,
-                    game: [...updatedData.all[i].game, curPlayerGameVal]
-                  }
-                : player;
-            })
-          };
-    }
-  }
-
-  // Assgin current player its data
-  const curPlayerData = updatedData.all.filter(player => player.id === curId);
-
-  return {
-    ...updatedData,
-    current: curPlayerData[0]
-  };
-};
-
+// If current player is the last player
+// return last player
+// else return next player
 export const getNextPlayer = ({
   all,
   current,
@@ -196,11 +63,7 @@ export const getNextPlayer = ({
   all: Array<ISinglePlayerObj>;
   current: ISinglePlayerObj;
   count: number;
-}) => {
-  // If current player is the last player
-  // return last player
-  // else return next player
-  return current.id === count
+}) =>
+  current.id === count
     ? all[0]
     : all.filter(player => player.id === current.id + 1)[0];
-};
