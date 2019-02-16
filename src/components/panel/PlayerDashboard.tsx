2975fbf1e5b46ac17356a84bb9b62b79af0559f6
Andy Tan <andytan0727@gmail.com>
2019-02-16 16:07:06 +0800

    Fix red font problem on unchange position.

--- src/components/panel/PlayerDashboard.tsx
+++ src/components/panel/PlayerDashboard.tsx
@@ -50,9 +50,11 @@ export default class PlayerDashboard extends React.PureComponent<
                           player.path[player.path.length - 2] &&
                           player.pos - player.path[player.path.length - 2] > 0
                             ? "green"
-                            : player.path.length === 1 || player.pos === 300
-                            ? "black"
-                            : "red"
+                            : player.path[player.path.length - 2] &&
+                              player.pos - player.path[player.path.length - 2] <
+                                0
+                            ? "red"
+                            : "black"
                       }}
                     >
                       {player.path[player.path.length - 2] || 1}{" "}
@@ -62,9 +64,11 @@ export default class PlayerDashboard extends React.PureComponent<
                           player.path[player.path.length - 2] &&
                           player.pos - player.path[player.path.length - 2] > 0
                             ? "green"
-                            : player.path.length === 1 || player.pos === 300
-                            ? "black"
-                            : "red"
+                            : player.path[player.path.length - 2] &&
+                              player.pos - player.path[player.path.length - 2] <
+                                0
+                            ? "red"
+                            : "black"
                         }
                         size={"large"}
                         verticalAlign={"middle"}
